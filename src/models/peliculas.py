40f463f2aeae5decb194724c469dd7f9f6a355c5
Lucas Robledo <l.robledo@itecriocuarto.org.ar>
2026-09-03 21:51:26 -0300

    Add files via upload

---
+++ src/models/peliculas.py
@@ -0,0 +1,11 @@
+from typing import Annotated
+from sqlalchemy import Boolean, Column, Integer, String
+
+
+class Pelicula(Base):
+    __tablename__ = "Peliculas"
+    id: Column(Integer,primary_key=True)
+    titulo: Column(String)
+    director: Column(String)
+    año: Column(Integer)
+    activo: Column(Boolean)
--- src/routers/peliculas.py
+++ src/routers/peliculas.py
@@ -1,7 +1,11 @@
 from typing import Annotated
-from fastapi import HTTPException, Path, APIRouter
-from schemas.peliculas import PeliculaSchema, PeliculaUpdate
 
+from fastapi import APIRouter, HTTPException, Path, Query, Depends
+from sqlalchemy.orm import Session
+from database import get_db
+
+from models.peliculas import Pelicula
+from schemas.peliculas import PeliculaSchema, PeliculaUpdate
 
 peliculas_routers = APIRouter()
 
@@ -18,51 +22,52 @@ NOT_FOUND_RESPONSE = {
     },
 }
 
-peliculas_db = [
-    {"id": 1, "titulo": "El Padrino", "director": "Francis Ford Coppola", "año": 1972},
-    {"id": 2, "titulo": "El Caballero Oscuro", "director": "Christopher Nolan", "año": 2008},
-    {"id": 3, "titulo": "Backrooms", "director": "Kane Person", "año": 2026},
-]
 
-@peliculas_routers.get("/")
-async def obtener_peliculas():
-    return peliculas_db
+@peliculas_routers.get("/", response_model=list[PeliculaSchema])
+async def obtener_peliculas(db: Session = Depends(get_db)):
+    peliculas = db.query(Pelicula).all()
+    return peliculas
 
-@peliculas_routers.get("/{id}", responses=NOT_FOUND_RESPONSE)
-async def obtener_pelicula(id: Annotated[int, Path(description="ID de la película")]):
-    for pelicula in peliculas_db:
-        if pelicula["id"] == id:
-            return pelicula
-    raise HTTPException(status_code=404, detail="Artículo no encontrado")
+@peliculas_routers.get("/{id}", responses=NOT_FOUND_RESPONSE, responses_model = PeliculaSchema)
+async def obtener_pelicula(id: Annotated[int, Path(gt=0)], db:Session = Depends(get_db)):
+    peliculas_obtenidas = db.get(Pelicula,id)
+    if peliculas_obtenidas is None:
+        return peliculas_obtenidas
+    else:
+        return HTTPException(status_code=404, detail="Articulo no encontrado")
 
-@peliculas_routers.post("/")
-async def crear_pelicula(pelicula: PeliculaSchema):
-    nueva_pelicula = {
-        "id": len(peliculas_db) + 1,
-        "titulo": pelicula.titulo,
-        "director": pelicula.director,
-        "año": pelicula.año,
-    }
-    peliculas_db.append(nueva_pelicula)
-    return nueva_pelicula
+@peliculas_routers.post("/", responses_model = PeliculaSchema)
+async def crear_pelicula(pelicula: PeliculaSchema, db:Session = Depends(get_db)):
 
-@peliculas_routers.put("/{id}", responses=NOT_FOUND_RESPONSE)
+    pelicula_db = Pelicula(
+        titulo = titulo_nuevo.titulo,
+        director = director_nuevo.director,
+        año = año_nuevo.año
+    )
+    db.add(pelicula_db)
+    db.commit()
+    db.refresh(pelicula_db)
+
+    return pelicula_db
+
+
+@peliculas_routers.put("/{id}", responses=NOT_FOUND_RESPONSE, response_model=(PeliculaSchema))
 async def actualizar_pelicula(
     id: Annotated[int, Path(gt=0, description="ID de la película a actualizar")],
     pelicula_update: PeliculaUpdate,
+    db:Session = Depends(get_db),
 ):
-    for pelicula in peliculas_db:
-        if pelicula["id"] == id:
-            pelicula["titulo"] = pelicula_update.titulo
-            pelicula["director"] = pelicula_update.director
-            pelicula["año"] = pelicula_update.año
-            return pelicula
-    raise HTTPException(status_code=404, detail="Artículo no encontrado")
 
+    pelicula_obtenidas = db.get(Pelicula,id)
+    if pelicula_obtenidas is not None:
+        pelicula_obtenidas.titulo = pelicula_editar.titulo
+        pelicula_obtenidas.director = pelicula_editar.director
+        pelicula_obtenidas.año = pelicula_editar.año
+        pelicula_obtenidas.activo = pelicula_editar.activo
+        db.commit()
+        db.refresh(pelicula_obtenidas)
+        return(pelicula_obtenidas)
+
+    raise HTTPException(status_code=404, detail="Articulo no encontrado")
 @peliculas_routers.delete("/{id}", responses=NOT_FOUND_RESPONSE)
-async def eliminar_pelicula(id: Annotated[int, Path(description="ID de la película a eliminar")]):
-    for pelicula in peliculas_db:
-        if pelicula["id"] == id:
-            peliculas_db.remove(pelicula)
-            return {"detail": "Pelicula eliminada"}
-    raise HTTPException(status_code=404, detail="Artículo no encontrado")
+async def eliminar_pelicula(id: Annotated[int, Path(gt=0)], db:Session = Depends(get_db)):
