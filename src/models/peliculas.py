2094843f3b7c53beac3b0a2ffe192a73097940c1
Lucas Robledo <l.robledo@itecriocuarto.org.ar>
2026-09-10 20:50:07 -0300

    Fix formatting of Pelicula class attributes

--- src/models/peliculas.py
+++ src/models/peliculas.py
@@ -4,8 +4,8 @@ from sqlalchemy import Boolean, Column, Integer, String
 
 class Pelicula(Base):
     __tablename__ = "Peliculas"
-    id: Column(Integer,primary_key=True)
-    titulo: Column(String)
-    director: Column(String)
-    año: Column(Integer)
-    activo: Column(Boolean)
+    id = Column(Integer,primary_key=True)
+    titulo = Column(String)
+    director = Column(String)
+    año = Column(Integer)
+    activo = Column(Boolean)
